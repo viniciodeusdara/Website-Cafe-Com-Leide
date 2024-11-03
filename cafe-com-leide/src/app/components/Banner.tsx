ce2735dc6add0e2d27f3bdf17b88a57dbc597cc4
ThalisAFalqueto <thalisambrosimfalqueto@gmail.com>
2024-11-03 18:01:04 -0300

    Banner criado

--- cafe-com-leide/src/app/components/Banner.tsx
+++ cafe-com-leide/src/app/components/Banner.tsx
@@ -1,7 +1,19 @@
-export default function Banner() {
-    return (
-      <div className="">
-        <h1>Banner</h1>
+// src/app/components/Banner.tsx
+
+import React from "react";
+import styles from "../styles/components/Banner.module.css";
+
+const Banner = () => {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.overlay}></div>{" "}
+      {/* Overlay para escurecer a imagem */}
+      <div className={styles.content}>
+        <h1>O seu pão de cada dia sempre fresquinho</h1>
+        <button className={styles.button}>Nossos pães</button>
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+export default Banner;
