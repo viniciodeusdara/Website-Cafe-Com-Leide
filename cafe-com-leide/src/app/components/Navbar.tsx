ca46970b9b92ab9778376e6e95cb12c9f5d16aaa
ThalisAFalqueto <thalisambrosimfalqueto@gmail.com>
2024-11-03 17:50:02 -0300

    navbar criada!

--- cafe-com-leide/src/app/components/Navbar.tsx
+++ cafe-com-leide/src/app/components/Navbar.tsx
@@ -1,7 +1,51 @@
-export default function Navbar() {
-    return (
-      <div className="">
-        <h1>Navbar</h1>
+// src/app/components/Navbar.tsx
+
+import React from "react";
+import Link from "next/link";
+import { FaInstagram, FaTiktok, FaFacebookF } from "react-icons/fa"; // Importação dos ícones
+import styles from "../styles/components/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.logo}>LOGO</div>
+
+      <ul className={styles.navLinks}>
+        <li>
+          <Link href="#about">Sobre</Link>
+        </li>
+        <li>
+          <Link href="#boulangerie">Boulangerie</Link>
+        </li>
+        <li>
+          <Link href="#products">Produtos</Link>
+        </li>
+        <li>
+          <Link href="#contact">Contato</Link>
+        </li>
+      </ul>
+
+      <div className={styles.socialIcons}>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram />
+        </a>
+        <a href="https://tiktok.com" target="_blank" rel="noopener noreferrer">
+          <FaTiktok />
+        </a>
+        <a
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebookF />
+        </a>
       </div>
-    );
-  }
+    </nav>
+  );
+};
+
+export default Navbar;
