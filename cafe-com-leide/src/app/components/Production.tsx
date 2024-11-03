be66ee496ae8b6f5953db2343574a15657173f0e
ThalisAFalqueto <thalisambrosimfalqueto@gmail.com>
2024-11-03 20:17:27 -0300

    add production

--- cafe-com-leide/src/app/components/Production.tsx
+++ cafe-com-leide/src/app/components/Production.tsx
@@ -1,7 +1,32 @@
-export default function Production() {
-    return (
-      <div className="">
-        <h1>Production</h1>
+// src/app/components/Production.tsx
+
+import React from "react";
+import styles from "../styles/components/Production.module.css";
+
+const Production = () => {
+  return (
+    <section className={styles.production}>
+      <div className={styles.videoContainer}>
+        <video className={styles.video} autoPlay loop muted>
+          <source src="/videos/production-video.mp4" type="video/mp4" />
+          Seu navegador não suporta o elemento de vídeo.
+        </video>
       </div>
-    );
-  }
+      <div className={styles.textContainer}>
+        <h2>
+          <span>Feitos à Mão, Com Amor e Cuidado</span>
+        </h2>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut nec
+          gravida metus. Duis consequat tortor ante. Etiam aliquet semper erat,
+          sed iaculis leo malesuada vitae. Sed non aliquam ex, nec vulputate
+          turpis. Mauris posuere dui et augue scelerisque, et tempor purus
+          bibendum. In mi sapien, elementum at luctus et, porta eu dui.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Production;
