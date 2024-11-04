9917a16a88f3991c647301425f47da1ba709ed25
Vinicio Deusdara <viniciodeusdara@gmail.com>
2024-11-03 21:35:49 -0300

    footer added

--- cafe-com-leide/src/app/components/Footer.tsx
+++ cafe-com-leide/src/app/components/Footer.tsx
@@ -1,7 +1,16 @@
-export default function Footer() {
-    return (
-      <div className="">
-        <h1>Footer</h1>
-      </div>
-    );
-  }
+import React from "react";
+import Link from "next/link";
+import styles from "@/app/styles/components/Footer.module.css";
+
+
+const Footer = () => {
+  return (
+    <section className={styles.footer}>
+  <h3>&copy;2024 -
+    <Link target="_blank" href="https://fgvjr.com/"> Developed by FGV JR</Link>
+  </h3>
+    </section>
+  );
+};
+
+export default Footer;
