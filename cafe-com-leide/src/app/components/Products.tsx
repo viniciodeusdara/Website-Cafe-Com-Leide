30147d845a1760fcc642c28f7bd692c2f5e504c1
RogerVin1 <roger.vini06@gmail.com>
2024-11-09 22:56:20 -0300

    Adição de comentários ao arquivo Products

--- cafe-com-leide/src/app/components/Products.tsx
+++ cafe-com-leide/src/app/components/Products.tsx
@@ -1,53 +1,71 @@
 import React from "react";
 import styles from "../styles/components/Products.module.css";
 
+// Componente Products que exibe uma galeria dos produtos da padaria
 const Products = () => {
     return (
-      <section id="products">
+      <section id="products"> {/* Seção com id "products" para navegação interna */}
         <div>
           <div className={styles.container}>
-        <div className={styles.header}>
-            <h1>NOSSOS PRODUTOS</h1>
-            <p>Descubra nossas delícias artesanais, feitas com ingredientes selecionados e muito carinho. São doces, bolos, tortas e sobremesas, criados para oferecer uma experiência única em cada mordida. Seja para uma ocasião especial ou para adoçar seu dia, nossos produtos trazem tradição e qualidade. Surpreenda-se com o sabor inigualável do artesanal!</p>
+            
+            {/* Cabeçalho da seção com título e descrição dos produtos */}
+            <div className={styles.header}>
+                <h1>NOSSOS PRODUTOS</h1> {/* Título da seção */}
+                <p>
+                  {/* Descrição dos produtos destacando os ingredientes selecionados e o toque artesanal */}
+                  Descubra nossas delícias artesanais, feitas com ingredientes selecionados e muito carinho. São doces, bolos, tortas e sobremesas, criados para oferecer uma experiência única em cada mordida. Seja para uma ocasião especial ou para adoçar seu dia, nossos produtos trazem tradição e qualidade. Surpreenda-se com o sabor inigualável do artesanal!
+                </p>
+            </div>
+
+            {/* Galeria de produtos com imagens e nomes dos itens */}
+            <div className={styles.gallery}>
+                
+                {/* Cada produto é representado como um item da galeria com uma imagem e uma legenda */}
+                
+                <div className={styles.item}>
+                    <img src="/images/italianinho.png" alt="Italianinho"/> {/* Imagem */}
+                    <span>Italianinho</span> {/* Nome do Produto */}
+                </div>
+                
+                <div className={styles.item}>
+                    <img src="/images/bolo_brigadeiro_gourmet.png" alt="Bolo de Brigadeiro Gourmet"/> {/* Imagem */}
+                    <span>Bolo de Brigadeiro Gourmet</span> {/* Nome do Produto */}
+                </div>
+                
+                <div className={styles.item}>
+                    <img src="/images/bolo_de_chocolate.png" alt="Bolo de chocolate"/> {/* Imagem */}
+                    <span>Bolo de chocolate</span> {/* Nome do Produto */}
+                </div>
+                
+                <div className={styles.item}>
+                    <img src="/images/torta_merengue_com_morangos.png" alt="Torta Merengue com morangos"/> {/* Imagem */}
+                    <span>Torta Merengue com morangos</span> {/* Nome do Produto */}
+                </div>
+                
+                <div className={styles.item}>
+                    <img src="/images/mini_naked_cake.png" alt="Mini Naked Cake"/> {/* Imagem */}
+                    <span>Mini Naked Cake</span> {/* Nome do Produto */}
+                </div>
+                
+                <div className={styles.item}>
+                    <img src="/images/rocambole_de_frutas_vermelhas.png" alt="Rocambole de frutas vermelhas"/> {/* Imagem */}
+                    <span>Rocambole de frutas vermelhas</span> {/* Nome do Produto */}
+                </div>
+                
+                <div className={styles.item}>
+                    <img src="/images/macarons.png" alt="Macarons"/> {/* Imagem */}
+                    <span>Macarons</span> {/* Nome do Produto */}
+                </div>
+                
+                <div className={styles.item}>
+                    <img src="/images/caneles.png" alt="Canelés"/> {/* Imagem */}
+                    <span>Canelés</span> {/* Nome do Produto */}
+                </div>
+            </div>
+          </div>
         </div>
-        <div className={styles.gallery}>
-            <div className={styles.item}>
-                <img src="/images/italianinho.png" alt="Italianinho"/>
-                <span>Italianinho</span>
-            </div>
-            <div className={styles.item}>
-                <img src="/images/bolo_brigadeiro_gourmet.png" alt="Bolo de Brigadeiro Gourmet"/>
-                <span>Bolo de Brigadeiro Gourmet</span>
-            </div>
-            <div className={styles.item}>
-                <img src="/images/bolo_de_chocolate.png" alt="Bolo de chocolate"/>
-                <span>Bolo de chocolate</span>
-            </div>
-            <div className={styles.item}>
-                <img src="/images/torta_merengue_com_morangos.png" alt="Torta Merengue com morangos"/>
-                <span>Torta Merengue com morangos</span>
-            </div>
-            <div className={styles.item}>
-                <img src="/images/mini_naked_cake.png" alt="Mini Naked Cake"/>
-                <span>Mini Naked Cake</span>
-            </div>
-            <div className={styles.item}>
-                <img src="/images/rocambole_de_frutas_vermelhas.png" alt="Rocambole de frutas vermelhas"/>
-                <span>Rocambole de frutas vermelhas</span>
-            </div>
-            <div className={styles.item}>
-                <img src="/images/macarons.png" alt="Macarons"/>
-                <span>Macarons</span>
-            </div>
-            <div className={styles.item}>
-                <img src="/images/caneles.png" alt="Canelés"/>
-                <span>Canelés</span>
-            </div>
-        </div>
-      </div>
-    </div>
       </section>
     );
-  };
+};
 
-export default Products
+export default Products;
