8dd7446f4779d957df2970b77a78cc43aaef3acc
RogerVin1 <roger.vini06@gmail.com>
2024-11-09 22:52:15 -0300

    Adição de comentários ao arquivo Navbar

--- cafe-com-leide/src/app/components/Navbar.tsx
+++ cafe-com-leide/src/app/components/Navbar.tsx
@@ -1,35 +1,46 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { FaInstagram, FaFacebook, FaTiktok } from "react-icons/fa";
+import { FaInstagram, FaFacebook, FaTiktok } from "react-icons/fa"; // Ícones de redes sociais
 import Link from "next/link";
 import Image from "next/image";
 import styles from "@/app/styles/components/Navbar.module.css";
 
+// Componente Navbar que exibe a barra de navegação do site com links para seções (para facilitar a locomoção pelo site) e para redes sociais
 const Navbar = () => {
 
-    // Adicionando efeito de mudança ao fazer o scroll da página
+    // Estado `scrolled` para aplicar efeitos quando a página é rolada
     const [scrolled, setScrolled] = useState(false);
 
+    // `useEffect` que adiciona um efeito para atualizar `scrolled` conforme o usuário rola a página, no caso o efeito é que a navbar acompanha o usuário durante toda a sua visita ao site
     useEffect(() => {
         const handleScroll = () => {
-            const isScrolled = window.scrollY > 50; 
+            const isScrolled = window.scrollY > 50; // Verifica se a rolagem é maior que 50 pixels
             setScrolled(isScrolled);
         };
 
-        window.addEventListener("scroll", handleScroll);
+        window.addEventListener("scroll", handleScroll); // Adiciona o evento de rolagem ao carregar o componente
         return () => {
-            window.removeEventListener("scroll", handleScroll);
+            window.removeEventListener("scroll", handleScroll); 
         };
     }, []);
 
-
-    // Estrutura da página
+    // Estrutura da Navbar com links internos e links para redes sociais
     return (
         <nav className={`${styles.menu} ${scrolled ? styles.scrolled : ""}`}>
+            
+            {/* Logo com link para a página inicial */}
             <Link href="/">
-                <Image className={styles.logo} src="/images/logo_no_bg.png" width={75} height={75} alt="Logo Café Com Leide" />
+                <Image 
+                    className={styles.logo} 
+                    src="/images/logo_no_bg.png" 
+                    width={75} 
+                    height={75} 
+                    alt="Logo Café Com Leide" 
+                />
             </Link>
+
+            {/* Links de navegação para seções do site */}
             <ul className={`${styles.buttons} ${scrolled ? styles.scrolled : ""}`}>
                 <li><Link href="/#about">Sobre</Link></li>
                 <li><Link href="/#boulangerie">Boulangerie</Link></li>
@@ -38,14 +49,30 @@ const Navbar = () => {
                 <li><Link href="/#contact">Contato</Link></li>
             </ul>
 
+            {/* Links para redes sociais com ícones personalizados */}
             <div className={`${styles.socials} ${scrolled ? styles.scrolled : ""}`}>
-                <Link target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/cafecomleide.ubatuba/" aria-label="Instagram">
+                <Link 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    href="https://www.instagram.com/cafecomleide.ubatuba/" 
+                    aria-label="Instagram"
+                >
                     <FaInstagram className={styles.iconInsta} />
                 </Link>
-                <Link target="_blank" rel="noopener noreferrer" href="https://www.tiktok.com/@cafecomleide.ubatuba" aria-label="TikTok">
+                <Link 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    href="https://www.tiktok.com/@cafecomleide.ubatuba" 
+                    aria-label="TikTok"
+                >
                     <FaTiktok className={styles.iconInsta} />
                 </Link>
-                <Link target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/cafecomleide.ubatuba" aria-label="Facebook">
+                <Link 
+                    target="_blank" 
+                    rel="noopener noreferrer" 
+                    href="https://www.facebook.com/cafecomleide.ubatuba" 
+                    aria-label="Facebook"
+                >
                     <FaFacebook className={styles.iconFace} />
                 </Link>
             </div>
