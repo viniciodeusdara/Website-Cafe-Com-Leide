14c874087aceee80f3425674d8e1849de480b915
RogerVin1 <roger.vini06@gmail.com>
2024-11-09 22:47:22 -0300

    Adição de comentários ao arquivo Footer

--- cafe-com-leide/src/app/components/Footer.tsx
+++ cafe-com-leide/src/app/components/Footer.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import Link from "next/link";
 import styles from "@/app/styles/components/Footer.module.css";
 
-
+// Componente Footer que exibe o rodapé do site com créditos de desenvolvimento à FGV JR
 const Footer = () => {
   return (
     <section className={styles.footer}>
-  <h3>&copy;2024 -
-    <Link target="_blank" href="https://fgvjr.com/"> Developed by FGV JR</Link>
-  </h3>
+      {/* Texto do rodapé com ano atual e link para o site da FGV JR */}
+      <h3>
+        &copy;2024 - 
+        <Link target="_blank" href="https://fgvjr.com/"> Developed by FGV JR</Link>
+      </h3>
     </section>
   );
 };
