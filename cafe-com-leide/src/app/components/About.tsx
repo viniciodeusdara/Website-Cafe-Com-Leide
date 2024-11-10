614f165eda74191e4ec0b20df90466b81804a5c7
RogerVin1 <roger.vini06@gmail.com>
2024-11-09 22:25:55 -0300

    Adição de comentários ao arquivo about

--- cafe-com-leide/src/app/components/About.tsx
+++ cafe-com-leide/src/app/components/About.tsx
@@ -1,27 +1,32 @@
-// src/app/components/About.tsx
-
+// Importa o React para construir o componente e o módulo de imagem do Next.js para otimização de imagens
 import React from "react";
 import Image from "next/image";
 import styles from "../styles/components/About.module.css";
 
+// Componente About que exibe uma seção de "Sobre Nós" com imagem e texto
 const About = () => {
   return (
+    // Define uma seção HTML com um id para navegação e aplica estilos específicos da seção "about"
     <section id="about" className={styles.about}>
       <div className={styles.content}>
-
+        
+        {/* Definição da Imagem */}
         <div className={styles.imageContainer}>
           <Image
-            src="/images/leide.png"
-            alt="Imagem da Leide segurando um pão italiano artesanal"
-            width={542}
-            height={672}
-            className={styles.image}
+            src="/images/leide.png"  // Caminho da imagem otimizada pelo Next.js
+            alt="Imagem da Leide segurando um pão italiano artesanal"  // Descrição da imagem para acessibilidade
+            width={542}  // Largura da imagem
+            height={672}  // Altura da imagem
+            className={styles.image}  // Estilos adicionais aplicados à imagem
           />
         </div>
+        
+        {/* Definição do Texto */}
         <div className={styles.textContainer}>
-          <h2>SOBRE NÓS</h2>
-          <h3>Fazendo produtos artesanais desde XXXX</h3>
+          <h2>SOBRE NÓS</h2>  {/* Título principal da seção */}
+          <h3>Fazendo produtos artesanais desde XXXX</h3>  {/* Subtítulo com o ano de início da produção */}
           <p>
+            {/* Parágrafo explicativo, com informações sobre a empresa */}
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
             laoreet sapien ut neque finibus commodo. Nunc tempus justo eu sem
             dignissim, quis tincidunt nisl tincidunt. Integer scelerisque enim
@@ -36,4 +41,5 @@ const About = () => {
   );
 };
 
+// Exporta o componente About para que possa ser usado em outras partes do site
 export default About;
