e526840a643273ab1b4016b15b4b8efeb6535925
RogerVin1 <roger.vini06@gmail.com>
2024-11-20 10:36:26 -0300

    Adicionei o efeito da navbar ao clicar no 'Hamburger'

--- cafe-com-leide/src/app/components/Navbar.tsx
+++ cafe-com-leide/src/app/components/Navbar.tsx
@@ -6,9 +6,6 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "@/app/styles/components/Navbar.module.css";
 
-
-
-
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
@@ -28,9 +25,13 @@ const Navbar = () => {
   const toggleMenu = () => {
     setMenuOpen((prev) => !prev);
   };
-  
+
   return (
-    <nav className={`${styles.menu} ${scrolled ? styles.scrolled : ""}`}>
+    <nav
+      className={`${styles.menu} ${
+        scrolled || menuOpen ? styles.scrolled : ""
+      }`}
+    >
       {/* Logo com link para a página inicial */}
       <Link href="/">
         <Image
@@ -62,38 +63,41 @@ const Navbar = () => {
       </ul>
 
       {/* Ícones de redes sociais e ícone de menu hambúrguer */}
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <div className={styles.socials}>
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.instagram.com/cafecomleide.ubatuba/"
-              aria-label="Instagram"
-            >
-              <FaInstagram />
-            </Link>
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.tiktok.com/@cafecomleide.ubatuba"
-              aria-label="TikTok"
-            >
-              <FaTiktok />
-            </Link>
-            <Link
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://www.facebook.com/cafecomleide.ubatuba"
-              aria-label="Facebook"
-            >
-              <FaFacebook />
-            </Link>
-          </div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <div className={styles.socials}>
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/cafecomleide.ubatuba/"
+            aria-label="Instagram"
+          >
+            <FaInstagram />
+          </Link>
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.tiktok.com/@cafecomleide.ubatuba"
+            aria-label="TikTok"
+          >
+            <FaTiktok />
+          </Link>
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/cafecomleide.ubatuba"
+            aria-label="Facebook"
+          >
+            <FaFacebook />
+          </Link>
+        </div>
 
-          {/* Ícone do menu hambúrguer (apenas no mobile) */}
-          <div className={`${styles.hamburger} ${menuOpen ? styles.active : ""}`} onClick={toggleMenu}>
-            <FaBars />
-          </div>
+        {/* Ícone do menu hambúrguer (apenas no mobile) */}
+        <div
+          className={`${styles.hamburger} ${menuOpen ? styles.active : ""}`}
+          onClick={toggleMenu}
+        >
+          <FaBars />
+        </div>
       </div>
     </nav>
   );
